94c4cd5e272c73aaa8b055a2ff910be5a39af223
Marius Müller <marius.mueller@sovanta.com>
2024-10-24 17:20:57 +0200

    feat: add keybinds, statusbar, mplexing

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,3 +1,9 @@
+-- TODO
+-- rename tab: leader %
+-- rename workspace
+-- create workspace with name
+-- resize better
+
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
@@ -7,10 +13,180 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("Iosevka Nerd Font Mono")
 config.font_size = 15
 
-config.enable_tab_bar = false
-
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
+
+-- tmux
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 5000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivateLastTab,
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "b",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "CTRL",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "CTRL",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "CTRL",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "CTRL",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "w",
+		action = wezterm.action.SpawnWindow,
+	},
+	{
+		mods = "LEADER",
+		key = "p",
+		action = wezterm.action.ActivateCommandPalette,
+	},
+	{
+		mods = "LEADER",
+		key = "s",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+	},
+	{
+		mods = "LEADER",
+		key = "e",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for workspace",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }))
+				end
+			end),
+		}),
+	},
+}
+
+local function shell(cmd)
+	local output = "null"
+	local f = io.popen(cmd)
+	if f ~= nil then
+		output = f:read("*all")
+		f:close()
+	end
+	return output
+end
+
+-- show workspace in statusbar
+wezterm.on("update-right-status", function(window, pane)
+	local workspace = window:active_workspace()
+	local bartib = shell(
+		"/usr/local/bin/bartib -f /Users/marius.mueller/notes/11\\ -\\ Sovanta/activities.bartib report --today | /usr/local/bin/rg 'Total(\\.)+ ([^\\n]+)' -r '$2'"
+	)
+	bartib = string.gsub(bartib, "\n", "")
+	window:set_right_status(wezterm.format({
+		{ Text = bartib .. " / " },
+		{ Text = workspace },
+	}))
+end)
+
+for i = 1, 9 do
+	-- leader + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
+end
+
+-- statusbar
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = false
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
+
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
 
 return config
