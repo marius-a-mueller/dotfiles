8c06f3594e10723a342f973e0c9917b6abafcc60
Marius Müller <marius.mueller@sovanta.com>
2024-10-30 15:10:13 +0100

    feat: change keybindings of telescope and version

--- .config/nvim/lua/main/lazy/telescope.lua
+++ .config/nvim/lua/main/lazy/telescope.lua
@@ -10,15 +10,9 @@ return {
 	config = function()
 		require("telescope").setup({})
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pp", builtin.git_files, { desc = "Telescope find git files" })
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({
-				search = vim.fn.input("Grep > "),
-				no_ignore = true,
-			})
-		end)
-		vim.keymap.set("n", "<leader>pf", function()
-			builtin.find_files({ no_ignore = true })
-		end)
+		vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "Telescope find git files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope find keymaps" })
 	end,
 }
--- .config/nvim/lua/main/remap.lua
+++ .config/nvim/lua/main/remap.lua
@@ -33,7 +33,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", function()
+vim.keymap.set("n", "<leader>fa", function()
 	vim.lsp.buf.format()
 end)
 
