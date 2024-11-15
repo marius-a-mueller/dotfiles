abfd5bc4d79f2d44207b957136bd45d4c5079861
Marius Müller <marius.mueller@sovanta.com>
2024-11-15 17:05:39 +0100

    feat(nvim): added commentary support for alloy files

--- .config/nvim/lua/main/lazy/commentary.lua
+++ .config/nvim/lua/main/lazy/commentary.lua
@@ -1,4 +1,9 @@
 return {
 	"tpope/vim-commentary",
-	config = function() end,
+	config = function()
+		-- set comment string for alloy files
+		vim.cmd([[
+          autocmd FileType alloy setlocal commentstring=//\ %s
+        ]])
+	end,
 }
