a417cf147de455c1cbf1c0f6155e10a0063b3205
Marius Müller <marius.mueller@sovanta.com>
2024-12-18 13:18:39 +0100

    feat(fish): add git fetch abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,6 +1,7 @@
 abbr --add --set-cursor gc git commit -m \'%\'
 abbr --add gs git status
 abbr --add ga git add
+abbr --add gf git fetch
 abbr --add gaa git add .
 abbr --add gco git checkout
 abbr --add gps git push
