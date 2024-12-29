2c6a485edf7da409267b9ebaa620b8a41ce8419c
Marius Müller <marius@bscnet.de>
2024-12-29 18:26:14 +0100

    fix(nix): split home-manager config

--- nixos/hosts/nixos/configuration.nix
+++ nixos/hosts/nixos/configuration.nix
@@ -114,33 +114,7 @@
   };
 
   home-manager.backupFileExtension = "backup";
-  home-manager.users.${vars.user} = { config, ...}: {
-    home = {
-      stateVersion = lib.mkDefault "22.05";
-      sessionPath = [
-        "/run/current-system/sw/bin"
-        "$HOME/.nix-profile/bin"
-      ];
-      file = {
-        ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/dotfiles/fish";
-        ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/dotfiles/wezterm";
-        ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/dotfiles/nvim";
-        ".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/dotfiles/starship.toml";
-        ".config/yazi".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/dotfiles/yazi";
-        ".config/eza".source = config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/dotfiles/eza";
-      };
-    };
-    programs = {
-      home-manager.enable = true;
-      neovim = {
-        enable = true;
-        defaultEditor = true;
-        vimAlias = true;
-        viAlias = true;
-      };
-      fish = {
-        enable = true;
-      };
-    };
-  };
+  home-manager.users.${vars.user}.imports = [
+    ./home-manager.nix
+  ];
 }
