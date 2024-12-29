66ec619c34275aab6c2b26fd1c17fbd821331918
Marius Müller <marius@bscnet.de>
2024-12-29 22:57:19 +0100

    feat(nix): set nixbox macaddress

--- nixos/hosts/nixos/nixbox/default.nix
+++ nixos/hosts/nixos/nixbox/default.nix
@@ -9,6 +9,7 @@
       (terraform.withPlugins (p: [ p.null p.external p.proxmox ]))
     ];
   };
+  networking.networkmanager.ethernet.macAddress = "bc:24:11:36:c7:8e";
 
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
