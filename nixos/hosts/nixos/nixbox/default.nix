2613dc62a06648ce38f7630dd189d034ba676edd
Marius Müller <marius@bscnet.de>
2024-12-29 22:47:03 +0100

    feat(nix): add grub config to nixbox

--- nixos/hosts/nixos/nixbox/default.nix
+++ nixos/hosts/nixos/nixbox/default.nix
@@ -9,4 +9,10 @@
       (terraform.withPlugins (p: [ p.null p.external p.proxmox ]))
     ];
   };
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
