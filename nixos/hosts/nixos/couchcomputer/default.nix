3614469444d80dc5951d01b0baf6b5f9cc7ee2f4
Marius Müller <marius@bscnet.de>
2024-12-29 18:05:40 +0100

    fix(nix): remove deprecated option for opengl

--- nixos/hosts/nixos/couchcomputer/default.nix
+++ nixos/hosts/nixos/couchcomputer/default.nix
@@ -5,13 +5,9 @@
   ];
 
   # https://medium.com/@notquitethereyet_/gaming-on-nixos-%EF%B8%8F-f98506351a24
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    # driSupport32bit = true;
-  };
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.open= true;
   # nix shell nixpkgs#pciutils -c lspci | grep VGA
   hardware.nvidia.prime = {
     offload = {
