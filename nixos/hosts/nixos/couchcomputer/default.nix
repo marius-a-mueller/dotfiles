d31d70878e44e2822bba9a4112ed9c5cbaa96e96
Marius Müller <marius@bscnet.de>
2025-01-05 18:11:56 +0100

    fix: remove duplicate systemPackages

--- nixos/hosts/nixos/couchcomputer/default.nix
+++ nixos/hosts/nixos/couchcomputer/default.nix
@@ -17,7 +17,6 @@
   hardware.nvidia.package = pkgs.linuxKernel.packages.linux_6_6.nvidia_x11;
   hardware.nvidia.modesetting.enable = true;
   # hardware.nvidia.prime.offload.enable = true;
-  environment.systemPackages = with pkgs; [ nvidia-docker ];
   # something broke though
   # services.xserver.dpi = 110;
   # environment.variables = { GDK_SCALE = "0.3"; };
