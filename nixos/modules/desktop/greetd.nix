9fc03e8d28a3ee4305255f7057db987273a8de72
Marius Müller <marius@bscnet.de>
2025-01-05 17:53:47 +0100

    fix: greetd config

--- nixos/modules/desktop/greetd.nix
+++ nixos/modules/desktop/greetd.nix
@@ -8,11 +8,14 @@
     services.greetd = {
       enable = true;
       settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --greeting 'Welcome to NixOS!' --cmd sway";
+          user = "${vars.user}";
+        };
         initial_session = {
           command = "${pkgs.sway}/bin/sway";
           user = "${vars.user}";
         };
-        default_session = initial_session;
       };
     };
 
