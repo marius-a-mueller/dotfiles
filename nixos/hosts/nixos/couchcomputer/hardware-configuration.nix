ee5159d70782917b581c199cf35be967e4aa7891
Marius Müller <marius@bscnet.de>
2025-01-05 16:36:12 +0100

    fix: cpu powersave mode

--- nixos/hosts/nixos/couchcomputer/hardware-configuration.nix
+++ nixos/hosts/nixos/couchcomputer/hardware-configuration.nix
@@ -33,6 +33,7 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  powerManagement.cpuFreqGovernor = "performance";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
