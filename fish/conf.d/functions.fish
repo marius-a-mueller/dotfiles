cabb0fdf3a12db3a854313c61e93f8775a9f7124
Marius Müller <marius@bscnet.de>
2025-01-08 12:03:52 +0100

    refactor: fish function rename

--- fish/conf.d/functions.fish
+++ fish/conf.d/functions.fish
@@ -1,3 +1,3 @@
-function nixos-rebuild -a flake host
+function nixos-deploy -a flake host
     /run/current-system/sw/bin/nixos-rebuild switch --flake $flake --target-host $host --build-host $host --fast --use-remote-sudo
 end
