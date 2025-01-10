81f86cb4e9811f586f7fa7e84615a7c2e908aa5f
Marius Müller <marius@bscnet.de>
2025-01-10 23:21:03 +0100

    fix(fish): correct path to nixos rebuild

--- fish/conf.d/functions.fish
+++ fish/conf.d/functions.fish
@@ -1,3 +1,3 @@
 function nixos-deploy -a flake host
-    /run/current-system/sw/bin/nixos-rebuild switch --flake $flake --target-host $host --build-host $host --fast --use-remote-sudo
+    nixos-rebuild switch --flake $flake --target-host $host --build-host $host --fast --use-remote-sudo
 end
