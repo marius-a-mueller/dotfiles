ca932df4135112aa2e0a43cc9ae9d65f7e0dd600
Marius Müller <marius@bscnet.de>
2025-01-13 11:58:48 +0100

    build: add sops-nix input

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -41,6 +41,11 @@
         inputs.nixpkgs.follows = "nixpkgs";
       };
 
+      sops-nix = {
+        url = "github:Mic92/sops-nix";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+
       # Fixes OpenGL With Other Distros.
       nixgl = {
         url = "github:guibou/nixGL";
