4f068f2f3a5ea371462e74cd44284962c24fd31f
Marius Müller <marius@bscnet.de>
2025-02-16 14:20:07 +0100

    fix: add gringotts to flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -112,7 +112,7 @@
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
               }
-              # inputs.disko.nixosModules.disko
+              inputs.disko.nixosModules.disko
             ];
           };
 
@@ -149,6 +149,7 @@
         helms-deep    = mkNixosConfiguration { hostname = "helms-deep"; };
         hogwash       = mkNixosConfiguration { hostname = "hogwash"; };
         nixbox        = mkNixosConfiguration { hostname = "nixbox"; };
+        gringotts     = mkNixosConfiguration { hostname = "gringotts"; };
       };
 
       darwinConfigurations = {
